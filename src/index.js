849d5ba5e030ea612cdf9ce50cea5ed8b154282d
Glendon Gengel <glendon.gengel@gmail.com>
2022-11-21 16:40:24 -0600

    Quick example of promises and .then functionality
    Spits out "bears beets battlestar galactica"

--- src/index.js
+++ src/index.js
@@ -5,3 +5,14 @@ import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+new Promise(resolve => {
+    setTimeout(() => {
+        console.log('Bears'); 
+        resolve();
+     }, 2000);     
+})
+.then(() => {
+    console.log('Beets');
+    console.log('Battlestar galactica');
+});
+
